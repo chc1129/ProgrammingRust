b2127c428ac41182415e441d0d4436dca7edb8a1
chc1129 <chc1129@gmail.com>
2023-09-09 20:36:34 +0900

    chap02 commit

---
+++ chap02/hello/target/debug/deps/hello-58159ab18a258e79.d
@@ -0,0 +1,5 @@
+/home/chicu/work/prg/rust/ProgrammingRust/chap02/hello/target/debug/deps/hello-58159ab18a258e79: src/main.rs
+
+/home/chicu/work/prg/rust/ProgrammingRust/chap02/hello/target/debug/deps/hello-58159ab18a258e79.d: src/main.rs
+
+src/main.rs:
